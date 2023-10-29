708915e34c6847d2554ca575e04f56bc9cc32ace
Ravindra Modpatel <ravi.rhce@gmail.com>
2023-10-29 02:10:42 +0000

    added cleanup file

---
+++ cleanup.sh
@@ -0,0 +1,14 @@
+kubectl delete deployments web-deployment -n staging
+kubectl delete namespaces staging
+kubectl delete  limitranges mem-limit-range -n haddock
+kubectl delete ns haddock
+kubectl delete cronjobs ppi
+kubectl delete pod nginx-fix
+kubectl delete secrets app-secret
+kubectl delete pod nginx-secret
+kubectl delete pod nginx-resources
+kubectl delete deployment appa -n frontend
+kubectl delete  sa app-service -n frontend
+kubectl delete ns frontend
+kubectl delete deployment apl -n ckad00014
+kubectl delete ns ckad00014
